8038b46edfefb40c70ef9ccb532d8c6375bb6998
gtct <92213730+gtct@users.noreply.github.com>
2023-12-19 19:10:06 +0100

    accidentally uploaded old files, fixed

--- GUI/script.js
+++ GUI/script.js
@@ -5,20 +5,20 @@ document.addEventListener('DOMContentLoaded', function () {
 function fetchData() {
     fetch('meals.json')
         .then(response => response.json())
-        .then(data => updateFields(data[0])) // Access the first meal in the array
+        .then(data => updateFields(data[4])) // Access the first meal in the array
         .catch(error => console.error('Error fetching data:', error));
 }
 
 function updateFields(data) {
-        // Replace content for the element with ID 'uniqueElement'
-    var uniqueElement = document.querySelector('#1a');
-    if (uniqueElement) {
-        uniqueElement.innerText = 'New Content for Unique Element';
-    }
-    
-        // Replace content for all elements with class 'commonElement'
-    var commonElements = document.querySelectorAll('.meal-title');
-    commonElements.forEach(function (element, index) {
-        element.innerText = 'New Content for Common Element ' + (index + 1);
+    // Iterate over all meal titles and change them accordingly
+    var titles = document.querySelectorAll('.meal-title');
+    titles.forEach(function (element, index) {
+        element.innerText = data.title;
+    });
+
+    // Iterate over meal photos (1st column only)
+    var photos = document.querySelectorAll('.meal-photo');
+    photos.forEach(function (element, index) {
+        element.src = data.photo;
     });
 }
